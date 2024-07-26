e95605adce9852208ae49cb4e1c52e1a5894d5f4
Cliff Ochieng <138933367+Cliffochi@users.noreply.github.com>
2024-07-26 12:04:50 +0300

    Add files via upload
    NashvilleHousing data cleaning using SQL script

---
+++ NashvilleHosing.sql
@@ -0,0 +1,178 @@
+/*
+This is a data cleaning project in SQL
+*/
+
+SELECT *
+FROM PortfolioProject..NashvilleHousing
+
+--standardizing the sale date format
+
+SELECT SaleDate, CONVERT (DATE, SaleDate) 
+FROM PortfolioProject..NashvilleHousing
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+ADD UpdatedSaleDate DATE;
+
+UPDATE PortfolioProject..NashvilleHousing
+SET UpdatedSaleDate = CONVERT(DATE, SaleDate)
+
+-- cleaning and formatting the address column
+
+SELECT NH1.ParcelID, NH1.PropertyAddress, NH2.ParcelID, NH2.PropertyAddress, ISNULL(NH1.PropertyAddress, NH2.PropertyAddress)
+FROM PortfolioProject..NashvilleHousing NH1
+JOIN PortfolioProject..NashvilleHousing NH2
+	ON NH1.ParcelID = NH1.ParcelID
+	AND NH1.[UniqueID ] <> NH2.[UniqueID ]
+WHERE NH1.PropertyAddress IS NULL --checking for the null values
+
+-- taking the prperty addresses in NH2 and replacing the NUll property addresses in NH1
+
+UPDATE NH1
+SET PropertyAddress = ISNULL(NH1.PropertyAddress, NH2.PropertyAddress)
+FROM PortfolioProject..NashvilleHousing NH1
+INNER JOIN PortfolioProject..NashvilleHousing NH2
+	ON NH1.ParcelID = NH2.ParcelID
+	AND NH1.[UniqueID ] <> NH2.[UniqueID ]
+WHERE NH1.PropertyAddress IS NULL
+
+--Breaking down the Property Address into Individual columns of state, city and address
+-- a quick scan reveals 2 types of delimiters in the PropertyAddress column i.e. space and comma
+
+SELECT PropertyAddress
+FROM PortfolioProject..NashvilleHousing
+
+SELECT 
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1) AS Address --taking up the values in the propertyaddress column excluding the comma
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, LEN(PropertyAddress)) AS Address
+FROM PortfolioProject..NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+ADD PropertySplitAddress NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1) 
+
+ALTER TABLE NashvilleHousing
+ADD PropertySplitCity NVARCHAR (255);
+
+UPDATE NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, LEN(PropertyAddress))
+
+--formatting the owner address to a usable format
+--this is a simpler method to sunstring used earlier
+SELECT OwnerAddress
+FROM PortfolioProject..NashvilleHousing
+
+SELECT
+PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3),
+PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2),
+PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+FROM PortfolioProject..NashvilleHousing
+
+--updating owneraddress on the table
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitAddress NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3)
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitCity NVARCHAR (255);
+
+UPDATE NashvilleHousing
+SET PropertySplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2)
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitState NVARCHAR (255);
+
+UPDATE NashvilleHousing
+SET PropertySplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+
+--homogenize the SoldAsVacant column to just Yes and No i.e. changing Y and N to Yes and No
+--check for the count of individual values
+
+SELECT DISTINCT(SoldAsVacant), COUNT(SoldAsVacant)
+FROM PortfolioProject..NashvilleHousing
+GROUP BY SoldAsVacant
+ORDER BY 2
+
+--using a case staement convert Ys and Ns
+
+SELECT SoldAsVacant,
+	CASE WHEN SoldAsVacant = 'Y' THEN 'YES'
+		 WHEN SoldAsVacant = 'N' THEN 'NO'
+		 ELSE SoldAsVacant
+		 END
+FROM PortfolioProject..NashvilleHousing
+
+UPDATE NashvilleHousing
+SET SoldAsVacant = CASE WHEN SoldAsVacant = 'Y' THEN 'YES'
+	WHEN SoldAsVacant = 'N' THEN 'NO'
+	ELSE SoldAsVacant
+	END
+
+--getting rid of duplicates
+--quick check for duplicates
+
+SELECT *,
+	ROW_NUMBER() OVER(
+	PARTITION BY ParcelId,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY UniqueId) row_num
+
+FROM PortfolioProject..NashvilleHousing
+ORDER BY ParcelId
+
+----
+--displaying only rows with duplicate entries
+
+WITH RowNumCTE AS (
+SELECT *,
+	ROW_NUMBER() OVER(
+	PARTITION BY ParcelId,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY UniqueId) row_num
+
+FROM PortfolioProject..NashvilleHousing 
+)
+
+SELECT *
+FROM RowNumCTE
+WHERE row_num > 1
+
+--Deleting the duplicate rows
+
+WITH RowNumCTE AS (
+SELECT *,
+	ROW_NUMBER() OVER(
+	PARTITION BY ParcelId,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY UniqueId) row_num
+
+FROM PortfolioProject..NashvilleHousing 
+)
+
+DELETE
+FROM RowNumCTE
+WHERE row_num > 1
+
+--deleting unused columns
+
+SELECT *
+FROM PortfolioProject..NashvilleHousing
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddres
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+DROP COLUMN SaleDate
